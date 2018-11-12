be1c08cb3c2a4cf0a94195d2459b8983e740d28b
Charlike Mike Reagent <mameto2011@gmail.com>
2018-11-12 15:27:07 +0200

    feat: export cli login as src/cli.js, allow --dry flag
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- cli.js
+++ cli.js
@@ -2,19 +2,11 @@
 
 'use strict';
 
-const fs = require('fs');
-const util = require('util');
-const path = require('path');
 const proc = require('process');
-
-const ded = require('dedent');
-const isCI = require('is-ci');
 const parser = require('mri');
-const getPkg = require('@tunnckocore/package-json');
-const { exec } = require('@tunnckocore/execa');
 
-const release = require('./index');
 const pkg = require('./package');
+const cli = require('./src/cli');
 
 const argv = parser(proc.argv.slice(2), {
   default: {
@@ -23,73 +15,4 @@ const argv = parser(proc.argv.slice(2), {
   },
 });
 
-getPkg(pkg.name)
-  .then(({ version }) => (version !== pkg.version ? version : false))
-  .catch((err) => {
-    console.debug(`Error getting package metadata from the registry: ${err}`);
-    console.debug("Don't worry, that is a check for latest cli version.");
-  })
-  .then((latestVersion) => {
-    if (latestVersion) {
-      console.log(`UPDATE AVAILABLE: v${latestVersion}`);
-      console.log(
-        'See:',
-        `https://github.com/${pkg.repository}/releases/tag/v${latestVersion}`,
-      );
-      console.log('');
-    }
-    if (argv.ci && !isCI) {
-      console.error('Publishing is only allowed on CI services!');
-      console.error(
-        'Try passing --no-ci flag to bypass this, if you are sure.',
-      );
-      proc.exit(1);
-    }
-  })
-  .then(() => release(argv))
-  .then(async (result) => {
-    if (!result.increment && !result.nextVersion) {
-      console.log('Skipping `npm publish` stage...');
-      return null;
-    }
-
-    const token = argv.token || proc.env.NPM_TOKEN;
-    if (!token) {
-      throw new Error(
-        'Expect --token to be passed or NPM_TOKEN environment variable to be set.',
-      );
-    }
-
-    console.log(
-      'Meta Info:',
-      JSON.stringify(
-        result,
-        (x, v) => (typeof v === 'function' ? '[Function]' : v),
-        2,
-      ),
-    );
-
-    const defaultRegistry = 'https://registry.npmjs.org/';
-    const registry = argv.registry || proc.env.NPM_REGISTRY || defaultRegistry;
-    const content = ded`//registry.npmjs.org/:_authToken=${token}
-    sign-git-tag=false
-    git-tag-version=false
-    allow-same-version=false
-    `;
-
-    const opts = {
-      cwd: argv.cwd,
-      stdio: 'inherit',
-    };
-
-    await util.promisify(fs.writeFile)(path.join(argv.cwd, '.npmrc'), content);
-    await exec(`npm version ${result.nextVersion}`, opts);
-    await exec(`npm publish --registry ${registry}`, opts);
-
-    console.log('Successfully published.');
-    return true;
-  })
-  .catch((err) => {
-    console.error(err.stack);
-    proc.exit(1);
-  });
+cli(pkg, argv).catch(console.error);
---
+++ src/cli.js
@@ -0,0 +1,93 @@
+import fs from 'fs';
+import util from 'util';
+import path from 'path';
+import proc from 'process';
+import getPkg from '@tunnckocore/package-json';
+import { exec } from '@tunnckocore/execa';
+import isCI from 'is-ci';
+import ded from 'dedent';
+
+import release from './index';
+
+export default function releaseCli(pkg, argv) {
+  return getPkg(pkg.name)
+    .then(({ version }) => (version !== pkg.version ? version : false))
+    .catch((err) => {
+      console.debug(`Error getting package metadata from the registry: ${err}`);
+      console.debug("Don't worry, that is a check for latest cli version.");
+    })
+    .then((latestVersion) => {
+      if (latestVersion) {
+        console.log(`UPDATE AVAILABLE: v${latestVersion}`);
+        console.log(
+          'See:',
+          `https://github.com/${pkg.repository}/releases/tag/v${latestVersion}`,
+        );
+        console.log('');
+      }
+      if (argv.ci && !isCI) {
+        console.error('Publishing is only allowed on CI services!');
+        console.error(
+          'Try passing --no-ci flag to bypass this, if you are sure.',
+        );
+        proc.exit(1);
+      }
+    })
+    .then(() => release(argv))
+    .then(async (result) => {
+      if (!result.increment && !result.nextVersion) {
+        console.log('Skipping `npm publish` stage...');
+        return null;
+      }
+
+      if (argv.dry) {
+        console.log(JSON.stringify(result, null, 2));
+        console.log(argv);
+        return null;
+      }
+
+      const token = argv.token || proc.env.NPM_TOKEN;
+      if (!token) {
+        throw new Error(
+          'Expect --token to be passed or NPM_TOKEN environment variable to be set.',
+        );
+      }
+
+      console.log(
+        'Meta Info:',
+        JSON.stringify(
+          result,
+          (x, v) => (typeof v === 'function' ? '[Function]' : v),
+          2,
+        ),
+      );
+
+      const defaultRegistry = 'https://registry.npmjs.org/';
+      const registry =
+        argv.registry || proc.env.NPM_REGISTRY || defaultRegistry;
+      const content = ded`//registry.npmjs.org/:_authToken=${token}
+    sign-git-tag=false
+    git-tag-version=false
+    allow-same-version=false
+    `;
+
+      const opts = {
+        cwd: argv.cwd,
+        stdio: 'inherit',
+      };
+
+      await util.promisify(fs.writeFile)(
+        path.join(argv.cwd, '.npmrc'),
+        content,
+      );
+      await exec(`npm version ${result.nextVersion}`, opts);
+      await exec(`npm publish --registry ${registry}`, opts);
+
+      console.log('Successfully published.');
+      return true;
+    })
+    .catch((err) => {
+      console.error(err.stack);
+      proc.exit(1);
+    });
+}
